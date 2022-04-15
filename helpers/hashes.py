6396ee05ee04aeb880658433eb9562d785069d88
filippolauria <filippo.lauria@iit.cnr.it>
2022-04-15 15:03:42 +0200

    Improved hashing helpers

--- helpers/hashes.py
+++ helpers/hashes.py
@@ -2,7 +2,7 @@
 #
 #  helpers/hashes.py
 #
-# Copyright 2021 Filippo Maria LAURIA <filippo.lauria@iit.cnr.it>
+# Copyright 2022 Filippo Maria LAURIA <filippo.lauria@iit.cnr.it>
 #
 # Institute of Informatics and Telematics (IIT)
 # Italian National Council of Research (CNR)
@@ -26,51 +26,92 @@
 #
 
 import hashlib
+import binascii
+
+
+class PasswordAttribute:
+    def gen(self, plaintext):
+        return str(plaintext)
+
+    def __init__(self, plaintext):
+        self.hash = self.gen(str(plaintext))
+
+    def check(self, hash_):
+        return self.hash == hash_
+
+    def __str__(self):
+        return self.hash
+
+
+class CleartextPassword(PasswordAttribute):
+    pass
+
+
+class MD5Password(PasswordAttribute):
+    def gen(self, plaintext):
+        return str(hashlib.md5(plaintext.encode()).hexdigest()).upper()
+
+
+class NTPassword(PasswordAttribute):
+    def gen(self, plaintext):
+        return str(binascii.hexlify(hashlib.new(
+            'md4', plaintext.encode('utf-16le')).digest()).upper().decode())
+
+
+class SHA1Password(PasswordAttribute):
+    def gen(self, plaintext):
+        return str(hashlib.sha1(plaintext.encode()).hexdigest()).upper()
+
+
+# maps some password radius attributes to the correct function
+allowed_hashing_methods_map = {
+    'Cleartext-Password': CleartextPassword,
+    'MD5-Password': MD5Password,
+    'NT-Password':  NTPassword,
+    'SHA1-Password': SHA1Password
+}
 
 # the list of the allowed hashing methods
-allowed_hashing_methods = ['MD5-Password', 'SHA1-Password', 'Cleartext-Password']
+allowed_hashing_methods = allowed_hashing_methods_map.keys()
+
+
+class PasswordAttributeFactory:
+    @classmethod
+    def create(cls, plaintext, attribute):
+        """ factory class for creating a (supported) password attribute"""
+
+        if attribute not in allowed_hashing_methods:
+            raise ValueError(f"Type {attribute} not supported")
+
+        if attribute == 'Cleartext-Password':
+            return CleartextPassword(plaintext)
+
+        if attribute == 'MD5-Password':
+            return MD5Password(plaintext)
+
+        if attribute == 'NT-Password':
+            return NTPassword(plaintext)
+
+        if attribute == 'SHA1-Password':
+            return SHA1Password(plaintext)
 
 
 def check_password_hash(password, hash_, hash_type):
     """
-    this function takes a cleartext passwords and returns true if it matches with the passed hash_,
-    hashed according the passed hash_type.
+    this function takes a cleartext passwords and returns true if it matches
+    with the passed hash_, hashed according the passed hash_type.
     If the hashed_type is not supported, it raises a ValueError exception
     """
 
-    password = str(password)
-    hash_ = str(hash_)
-
-    if hash_type == 'Cleartext-Password':
-        return password == hash_
-
-    if hash_type == 'MD5-Password':
-        h = str(hashlib.md5(password.encode()).hexdigest()).lower()
-        hash_ = hash_.lower()
-        return h == hash_
-
-    if hash_type == 'SHA1-Password':
-        h = str(hashlib.sha1(password.encode()).hexdigest()).lower()
-        hash_ = hash_.lower()
-        return h == hash_
-
-    raise ValueError(f"Type {hash_type} not supported")
+    hash_obj = PasswordAttributeFactory.create(password, hash_type)
+    return hash_obj.check(hash_)
 
 
 def make_hash(password, hash_type):
     """
-    this function returns the hashed value (according to hash_type) associated with the passed password.
-    If the hashed_type is not supported, it raises a ValueError exception
+    this function returns the hashed value (according to hash_type)
+    associated with the passed password. If the hashed_type is not supported,
+    it raises a ValueError exception
     """
-    password = str(password)
-
-    if hash_type == 'Cleartext-Password':
-        return password
-
-    if hash_type == 'MD5-Password':
-        return str(hashlib.md5(password.encode()).hexdigest()).lower()
-
-    if hash_type == 'SHA1-Password':
-        return str(hashlib.sha1(password.encode()).hexdigest()).lower()
-
-    raise ValueError(f"Type {hash_type} not supported")
+    hash_obj = PasswordAttributeFactory.create(password, hash_type)
+    return str(hash_obj)
